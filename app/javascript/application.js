a82d289bdc112952244a579badeac43c4702e61c
Igor Renan <igorenansc@gmail.com>
2022-04-30 08:45:38 -0300

    Add navigation and enabled dropdown

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "jquery" 
+import "jquery"
 import "semantic-ui"
 
-$(document).ready(function(){$('.ui.accordion').accordion()});
+$('.ui.dropdown')
+  .dropdown()
+  ;
